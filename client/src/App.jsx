d625fad5b56dfff47761c7285ef5353457dc2ac2
johnsonmkevin <harlemworld1985@gmail.com>
2023-03-07 23:49:46 +0100

    Some css changes to links and business icons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,81 +6,72 @@ import SearchSharpIcon from "@mui/icons-material/SearchSharp";
 import AccountCircleSharpIcon from "@mui/icons-material/AccountCircleSharp";
 
 function App() {
-  // Connection String and socket
-  const ws = new WebSocket("ws://localhost:8000/");
+	// Connection String and socket
+	const ws = new WebSocket("ws://localhost:8000/");
 
-  // re render on every change
-  useEffect(() => {
-    ws.onopen = (res) => console.log("OPEN CONNECTION ====>", res);
-    ws.onmessage = (res) =>
-      console.log("MESSAGE FROM BACKEND ====>", JSON.parse(res.data));
-  }, []);
+	// re render on every change
+	useEffect(() => {
+		ws.onopen = (res) => console.log("OPEN CONNECTION ====>", res);
+		ws.onmessage = (res) => console.log("MESSAGE FROM BACKEND ====>", JSON.parse(res.data));
+	}, []);
 
-  // sends expected data that we want to receive
-  const sendData = () => {
-    ws.send(
-      JSON.stringify({ type: "MineService", action: "LOAD_ALL_CONNECTIONS" })
-    );
-  };
+	// sends expected data that we want to receive
+	const sendData = () => {
+		ws.send(JSON.stringify({ type: "MineService", action: "LOAD_ALL_CONNECTIONS" }));
+	};
 
-  return (
-    <div className="App">
-      <div className="header">
-        <h1>Mosaiq</h1>
-      </div>
-      <div className="menu">
-        <div className="dashboardBar">
-          <div>
-            <HomeIcon /> /miner/Jobs/Hi_Test_Job_Creation
-          </div>
-          <div className="searchContainer">
-            <form action="/action_page.php">
-              <input type="text" placeholder="Search.." name="search" />
-              <button type="submit">
-                <i className="fafa-search">
-                  <SearchSharpIcon />
-                </i>
-              </button>
-            </form>
-            <div>
-              <AccountCircleSharpIcon /> HI_Miner
-            </div>
-          </div>
-        </div>
-        <div className="menuWrapper">
-          <ul>
-            <li>
-              <a href="">HOME</a>
-            </li>
-            <li>
-              <a href="">CONNECTIONS</a>
-            </li>
-            <li>
-              <a href="">JOBS</a>
-            </li>
-            <li>
-              <a href="">ALERTS</a>
-            </li>
-            <li>
-              <a href="">ADMIN TOOLS</a>
-            </li>
-            <li>
-              <a href="">SUPPORT</a>
-            </li>
-            <li>
-              <a href="">SETTINGS</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="dashboard">
-        <Card
-          title="cardTitle"
-          body="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
-        />
-      </div>
-      <button onClick={sendData}>Send Data</button>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="header">
+				<h1>mosaiq</h1>
+			</div>
+			<div className="menu">
+				<div className="dashboardBar">
+					<div>
+						<HomeIcon /> /miner/Jobs/Hi_Test_Job_Creation
+					</div>
+					<div className="searchContainer">
+						<form action="/action_page.php">
+							<input type="text" placeholder="Search.." name="search" />
+							<button type="submit">
+								<i className="fafa-search">
+									<SearchSharpIcon />
+								</i>
+							</button>
+						</form>
+						<div>
+							<AccountCircleSharpIcon /> HI_Miner
+						</div>
+					</div>
+				</div>
+				<div className="menuWrapper">
+					<ul>
+						<li>
+							<a href="">Home</a>
+						</li>
+						<li>
+							<a href="">Connections</a>
+						</li>
+						<li>
+							<a href="">Jobs</a>
+						</li>
+						<li>
+							<a href="">Alerts</a>
+						</li>
+						<li>
+							<a href="">Help Center</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className="dashboard">
+				<Card
+					title="cardTitle"
+					body="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
+				/>
+			</div>
+			<button onClick={sendData}>Send Data</button>
+		</div>
+	);
 }
 export default App;
