c421166d16e70c6e1cad08f848baa1c45e6fa162
Willi0te <william.sinclair92@gmail.comm>
2023-03-08 12:08:25 +0100

    uppdated the dashboard card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,77 +1,87 @@
 import { useEffect } from "react";
 import "./App.css";
-import Card from "../src/components/Card.js";
+import StatusCard from "./components/StatusCard.js";
 import HomeIcon from "@mui/icons-material/Home";
 import SearchSharpIcon from "@mui/icons-material/SearchSharp";
 import AccountCircleSharpIcon from "@mui/icons-material/AccountCircleSharp";
 
 function App() {
-	// Connection String and socket
-	const ws = new WebSocket("ws://localhost:8000/");
+  // Connection String and socket
+  const ws = new WebSocket("ws://localhost:8000/");
 
-	// re render on every change
-	useEffect(() => {
-		ws.onopen = (res) => console.log("OPEN CONNECTION ====>", res);
-		ws.onmessage = (res) => console.log("MESSAGE FROM BACKEND ====>", JSON.parse(res.data));
-	}, []);
+  // re render on every change
+  useEffect(() => {
+    ws.onopen = (res) => console.log("OPEN CONNECTION ====>", res);
+    ws.onmessage = (res) =>
+      console.log("MESSAGE FROM BACKEND ====>", JSON.parse(res.data));
+  }, []);
 
-	// sends expected data that we want to receive
-	const sendData = () => {
-		ws.send(JSON.stringify({ type: "MineService", action: "LOAD_ALL_CONNECTIONS" }));
-	};
+  // sends expected data that we want to receive
+  const sendData = () => {
+    ws.send(
+      JSON.stringify({ type: "MineService", action: "LOAD_ALL_CONNECTIONS" })
+    );
+  };
 
-	return (
-		<div className="App">
-			<div className="header">
-				<h1>mosaiq</h1>
-			</div>
-			<div className="menu">
-				<div className="dashboardBar">
-					<div>
-						<HomeIcon /> /miner/Jobs/Hi_Test_Job_Creation
-					</div>
-					<div className="searchContainer">
-						<form action="/action_page.php">
-							<input type="text" placeholder="Search.." name="search" />
-							<button type="submit">
-								<i className="fafa-search">
-									<SearchSharpIcon />
-								</i>
-							</button>
-						</form>
-						<div>
-							<AccountCircleSharpIcon /> HI_Miner
-						</div>
-					</div>
-				</div>
-				<div className="menuWrapper">
-					<ul>
-						<li>
-							<a href="">Home</a>
-						</li>
-						<li>
-							<a href="">Connections</a>
-						</li>
-						<li>
-							<a href="">Jobs</a>
-						</li>
-						<li>
-							<a href="">Alerts</a>
-						</li>
-						<li>
-							<a href="">Help Center</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-			<div className="dashboard">
-				<Card
-					title="cardTitle"
-					body="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
-				/>
-			</div>
-			<button onClick={sendData}>Send Data</button>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>mosaiq</h1>
+      </div>
+      <div className="menu">
+        <div className="dashboardBar">
+          <div>
+            <HomeIcon /> /miner/Jobs/Hi_Test_Job_Creation
+          </div>
+          <div className="searchContainer">
+            <form action="/action_page.php">
+              <input type="text" placeholder="Search.." name="search" />
+              <button type="submit">
+                <i className="fafa-search">
+                  <SearchSharpIcon />
+                </i>
+              </button>
+            </form>
+            <div>
+              <AccountCircleSharpIcon /> HI_Miner
+            </div>
+          </div>
+        </div>
+        <div className="menuWrapper">
+          <ul>
+            <li>
+              <a href="">Home</a>
+            </li>
+            <li>
+              <a href="">Connections</a>
+            </li>
+            <li>
+              <a href="">Jobs</a>
+            </li>
+            <li>
+              <a href="">Alerts</a>
+            </li>
+            <li>
+              <a href="">Help Center</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="dashboard">
+        <StatusCard
+          title1="On/Off"
+          title2="Type"
+          title3="Status"
+          title4="Last reading"
+          title5="Config"
+          swtich="On"
+          type="SMHI"
+          status="Active"
+          lastReading="2023-02-25"
+        />
+      </div>
+      <button onClick={sendData}>Send Data</button>
+    </div>
+  );
 }
 export default App;
---
+++ client/src/components/StatusCard.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./StatusCard.css";
+import DownloadIcon from "@mui/icons-material/Download";
+
+function Card({
+  title1,
+  title2,
+  title3,
+  title4,
+  title5,
+  swtich,
+  type,
+  status,
+  lastReading,
+}) {
+  return (
+    <div className="cardContainer">
+      <div className="titleContainer">
+        <div className="card-title">
+          <h3>{title1}</h3>
+          <div className="switch">{swtich}</div>
+        </div>
+      </div>
+      <div className="titleContainer">
+        <div className="card-title">
+          <h3>{title2}</h3>
+          <div className="type">{type}</div>
+        </div>
+      </div>
+      <div className="titleContainer">
+        <div className="card-title">
+          <h3>{title3}</h3>
+          <div className="status">{status}</div>
+        </div>
+      </div>
+      <div className="titleContainer">
+        <div className="card-title">
+          <h3>{title4}</h3>
+          <div className="lastReading">{lastReading}</div>
+        </div>
+      </div>
+      <div className="titleContainer">
+        <div className="card-title">
+          <h3>{title5}</h3>
+          <div>
+            <DownloadIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
